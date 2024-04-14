41b4f27deb230345704eb53aedf2322ad0510d44
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-04-15 01:36:49 +0530

    Time: 0 ms (100.00%), Space: 14.8 MB (48.66%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -1,24 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sum(TreeNode* curr, TreeNode* parent) {
-        if(!curr)
+    int solve(TreeNode* curr,TreeNode* parent){
+        if(curr==NULL){
             return 0;
-        
-        int left  = sum(curr->left, curr);
-        int right = sum(curr->right, curr);
-        
-        int sum = 0;
-        if(curr->left == NULL && curr->right == NULL) {
-            if(parent && parent->left == curr)
-                sum += curr->val;
         }
-        
-        return left+right+sum;
+        if(curr->right==NULL&& curr->left==NULL){
+            if(parent!=NULL && parent->left==curr){
+                return curr->val;            }
+        }
+        int left=solve(curr->left,curr);
+        int right=solve(curr->right,curr);
+        return left+right;
     }
     int sumOfLeftLeaves(TreeNode* root) {
-        if(!root)
-            return 0;
-        TreeNode* parent = NULL;
-        return sum(root, parent);
+        return solve(root,NULL);
     }
 };
