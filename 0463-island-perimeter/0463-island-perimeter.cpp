17870a8d0edb481dd5214f58d358a475e44173e2
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-04-19 00:59:45 +0530

    Time: 99 ms (11.33%), Space: 108.3 MB (7.41%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
-    int m;
-    int n;
     int peri;
-    
-    void dfs(vector<vector<int>>& grid, int i, int j) {
-        
-        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
-            peri++;
-            return;
+    int n;
+    int m;
+    vector<vector<int>>direction{{1,0},{-1,0},{0,1},{0,-1}};
+    int dfs(vector<vector<int>>& grid,int i,int j){
+        queue<pair<int,int>>que;
+        que.push({i,j});
+        grid[i][j]=-1;
+        while(!que.empty()){
+            auto it=que.front();
+            que.pop();
+            for(auto &dir:direction){
+                int i_=it.first+dir[0];
+                int j_=it.second+dir[1];
+                if(i_<0 || i_>=m || j_<0 || j_>=n || grid[i_][j_]==0){
+                    peri++;
+                }
+                else if(grid[i_][j_]==-1){
+                    continue;
+                }
+                else{
+                    que.push({i_,j_});
+                    grid[i_][j_]=-1;
+                    
+                }
+                
+            }
         }
-        
-        if(grid[i][j] == -1) {
-            return;
-        }
-        
-        grid[i][j] = -1; //mark visited
-        
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
-        
-    }
-    
+        return peri;
+    }    
     int islandPerimeter(vector<vector<int>>& grid) {
         m    = grid.size();
-        n    = grid[0].size();
+         n    = grid[0].size();
         peri = 0;
         
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 
                 if(grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                    return peri;
+                   return  dfs(grid, i, j);
                 }
                 
             }
