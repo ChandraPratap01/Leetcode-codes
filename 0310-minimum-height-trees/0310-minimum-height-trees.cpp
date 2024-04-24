0bfc272d15892147bf9af91ac29d7d38c55ff53d
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-04-24 13:22:25 +0530

    Time: 129 ms (41.20%), Space: 63.1 MB (38.62%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n == 1)
+        if(n==1){
             return {0};
-        
-        vector<int> result;
-        vector<int> indegree(n);
-        map<int, vector<int>> adj;
-        
-        for(vector<int> vec:edges) {
-            int u = vec[0];
-            int v = vec[1];
+        }
+        unordered_map<int,vector<int>>mp;
+        vector<int>indegree(n);
+        for(auto &it:edges){
+            int u=it[0];
+            int v=it[1];
             indegree[u]++;
             indegree[v]++;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            mp[u].push_back(v);
+            mp[v].push_back(u); 
         }
-        
-        queue<int> que;
-        for(int i = 0; i<n; i++) {
-            if(indegree[i] == 1) //adding all leafs node so that we can remove them later
+        queue<int>que;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==1){
                 que.push(i);
+            }
         }
-        
-        while(n > 2) {
-            int size = que.size();
-            n -= size; //removing nodes with indegreee 1
-            
-            while(size--) {
-                int u = que.front();
+        while(n>2){
+            int size=que.size();
+            n-=size;
+            while(size--){
+                int u=que.front();
                 que.pop();
-                for(int v:adj[u]) {
+                indegree[u]--;
+                for(auto &v:mp[u]){
                     indegree[v]--;
-                    if(indegree[v] == 1)
+                    if(indegree[v]==1){
                         que.push(v);
+                    }
                 }
             }
         }
-        
-        while(!que.empty()) {
+        vector<int>result;
+        while(!que.empty()){
             result.push_back(que.front());
             que.pop();
         }
