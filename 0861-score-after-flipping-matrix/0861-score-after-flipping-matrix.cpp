b21474788a9f107a4f2b288b7a766ba9dd5aa8cc
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-05-14 00:48:23 +0530

    Time: 5 ms (31.84%), Space: 10.3 MB (37.53%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -3,36 +3,20 @@ public:
     int matrixScore(vector<vector<int>>& grid) {
         int row=grid.size();
         int column=grid[0].size();
-        for(int i=0;i<row;i++){
-            if(grid[i][0]==0){
-                for(int j=0;j<column;j++){
-                    grid[i][j]=1-grid[i][j];
-                }
-            }
-        }
-        for(int i=1;i<column;i++){
-             int zeroes=0;
+        int sum=0;
+        sum+=pow(2,column-1)*row;
+         for(int i=1;i<column;i++){
              int ones=0;
+              int zeroes=0;
             for(int j=0;j<row;j++){
-                if(grid[j][i]==0){
-                    zeroes++;
+                if(grid[j][i]==grid[j][0]){
+                  ones++;
                 }
             }
-            ones=row-zeroes;
-            if(zeroes>ones){
-                for(int k=0;k<row;k++){
-                    grid[k][i]=1-grid[k][i];
-                }
-            }
-        }
-        int result=0;
-        for(int i=0;i<row;i++){
-            int score=0;
-            for(int j=0;j<column;j++){
-               score=grid[i][j]*pow(2,column-j-1);
-                result+=score;
-            }
-        }
-        return result;
+            zeroes=row-ones;
+            int  countones=max(zeroes,ones);
+                 sum+=pow(2,column-i-1)*countones;
+         }
+        return sum;
     }
 };
