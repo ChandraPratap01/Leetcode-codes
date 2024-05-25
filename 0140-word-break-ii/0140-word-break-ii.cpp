486967b5108243ffd671274b83304da6bb694a91
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-05-26 01:49:47 +0530

    Time: 4 ms (32.08%), Space: 8.9 MB (66.68%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string>result;
+    int n;
+    unordered_set<string>st;
+     void solve(int i,string s,string &currstring){
+        if(i>=n){
+            result.push_back(currstring);
+        }
+        for(int j=i;j<n;j++){
+           string check=s.substr(i,j-i+1);
+             if(st.count(check)){
+                 string cmp=currstring;
+                 if(!currstring.empty()){
+                     currstring+=" ";
+                 }
+                 currstring+=check;
+                 solve(j+1,s,currstring);
+                 currstring=cmp;
+                 
+             }
+        }
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        n=s.size();
+        for(string &St:wordDict){
+            st.insert(St);
+        }
+        string currstring="";
+        solve(0,s,currstring);
+            return result;
+    }
+};
