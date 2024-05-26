c930f87cf972984c5c72f8ad290b7618fcba521d
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-05-27 01:23:26 +0530

    Time: 40 ms (87.21%), Space: 9.8 MB (88.75%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
- int M=1e9+7;
-    int t[100001][2][3];
-    int solve(int n,int absent,int late){
-  if(absent>1 || late>=3){
-            return 0;
-        }
-        if(n==0){
-            return 1;
-        }
-        if(t[n][absent][late]!=-1){
-            return t[n][absent][late];
-        }
-        int A=solve(n-1,absent+1,0);
-        int B=solve(n-1,absent,late+1);
-        int C=solve(n-1,absent,0);
-            return t[n][absent][late]=((A+B)%M+C)%M;
-    }
+    int M=1e9+7;
     int checkRecord(int n) {
-        memset(t,-1,sizeof(t));
-        return solve(n,0,0);  
+int t[100001][2][3];
+        for(int i=0;i<=n;i++){
+            for(int A=0;A<=1;A++){
+                for(int L=0;L<=2;L++){
+                    if(i==0){
+                        t[0][A][L]=1;
+                    }
+                    else{
+                    long ans=t[i-1][A][0]%M;
+                    ans+=(L+1<=2 ? t[i-1][A][L+1]:0);
+                    ans+=(A+1<=1?t[i-1][A+1][0]:0);
+                        t[i][A][L]=ans%M;
+                    }
+                    
+                }
+            }
+        }
+        return t[n][0][0];
     }
 };
