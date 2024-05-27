f12eb0c703865a24deb0cd5cc43a723de1c6405c
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-05-28 02:45:16 +0530

    Time: 5 ms (33.10%), Space: 10.5 MB (9.31%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-      sort(nums.begin(),nums.end())  ;
-        for(int i=nums.size();i>=1;i--){
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]>=i){
-                    int k=nums.size()-j;
-                    if(k==i){
-                        return i;
-                    }
-                    else{
-                        break;
-                    }
-                }
+        int n=nums.size();
+        vector<int>freq(n+1);
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            freq[min(n,num)]++;
+        }
+        int cumm_sum=0;
+        for(int x=n;x>=0;x--){
+            cumm_sum+=freq[x];
+            if(cumm_sum==x){
+                return x;
             }
         }
         return -1;
