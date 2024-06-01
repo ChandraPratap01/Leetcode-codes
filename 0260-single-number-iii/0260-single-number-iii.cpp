d5045b61808871a6b586a3985e29b837c03c697c
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-06-01 13:11:00 +0530

    Time: 10 ms (38.10%), Space: 12.4 MB (64.88%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        long long xor_r = 0;
-
-        for(int &num : nums) {
-            xor_r ^= num;
+        long long xor_=0;
+        for(int &num:nums){
+            xor_^=num;
         }
-
-        //mask -> right most set bit search 
-        int mask = (xor_r) & (-xor_r);
-        /*
-            It works because if you take the two's complement negation of a number, first you complement it,
-            setting all zeroes to the right of the lowest set bit to one and the lowest set bit to zero,
-            then you add one, setting the bits on the right to zero and the lowest set bit becomes one again, ending the carry chain.
-        */
-
-        int groupa = 0;
-        int groupb = 0;
-
-        for(int &num : nums) {
-            if(num & mask) {
-                groupa ^= num;
-            } else {
-                groupb ^= num;
+     //   mask->right most bit(1) search;
+        int mask=xor_ &(-xor_);
+        int groupA=0;
+        int groupB=0;
+        for(int &num:nums){
+            if(num&mask){
+                groupA^=num;
+            }else{
+                groupB^=num;
             }
         }
-
-        return {groupa, groupb};
+        return {groupA,groupB};
     }
 };
