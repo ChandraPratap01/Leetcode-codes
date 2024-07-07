90861d8a5464d356a7d359543a65736cb5f96e9b
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-07-08 00:44:01 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (95.58%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int consumed_water=0;
-        int emptywater=0;
-        while(numBottles>=numExchange){
-            consumed_water+=numExchange;
-             numBottles-=numExchange;
-            numBottles+=1;
+        int consumed=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange){
+            int extrafullwater=empty/numExchange;
+             int remain=empty%numExchange;
+            consumed+=extrafullwater;
+            empty=extrafullwater+remain;
+                
         }
-        return consumed_water+numBottles;
+        return consumed;
     }
 };
