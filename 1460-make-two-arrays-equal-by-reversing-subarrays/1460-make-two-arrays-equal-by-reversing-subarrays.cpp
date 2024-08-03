687ff29b56637af62fdb8dd3a676060a0a94d1b8
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-08-04 01:38:41 +0530

    Time: 5 ms (88.20%), Space: 19.6 MB (21.06%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        unordered_map<int,int>mp;
+        int n=arr.size();
+        for(int i:arr){
+            mp[i]++;
+        }
+        for(int i=0;i<n;i++){
+            if(mp[target[i]]>0){
+                mp[target[i]]--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
