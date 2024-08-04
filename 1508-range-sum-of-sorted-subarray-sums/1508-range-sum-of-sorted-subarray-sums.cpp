7008209aa057fedaa3fa8ff862d704d11b596175
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-08-05 00:38:44 +0530

    Time: 70 ms (88.54%), Space: 11.6 MB (77.98%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int M=1e9+7;
+    typedef pair<int,int>P;//{sum,index}
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<int>result;
+        priority_queue<P,vector<P>,greater<P>>pq;
         for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                result.push_back(sum);
+            pq.push({nums[i],i});
+        }
+        int result=0;
+        for(int i=1;i<=right;i++){
+            auto it=pq.top();
+            pq.pop();
+            int sum=it.first;
+            int idx=it.second;
+            if(i>=left){
+                result=(result+sum)%M;
+            }
+            int new_idx=idx+1;
+            if(new_idx<n){
+                P new_pair={sum+nums[new_idx],new_idx};
+                pq.push(new_pair);
             }
         }
-        sort(result.begin(),result.end());
-        int sum=0;
-        for(int i=left-1;i<right;i++){
-            sum=(sum+result[i])%M;
-        }
-        return sum;
+        return result;
     }
 };
