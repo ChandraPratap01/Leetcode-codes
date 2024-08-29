06c317428541f340176226dbc8c56be7d4be1587
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-08-29 17:01:09 +0530

    Time: 208 ms (75.96%), Space: 95.8 MB (57.68%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-
-    //DFS
-    bool checkSubIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
-        if(i < 0 || i >= grid1.size() || j < 0 || j >= grid1[0].size()) {
+    bool substring(vector<vector<int>>& grid1,vector<vector<int>>& grid2,int i,int j){
+        if(i<0 || i>=grid1.size() || j<0 ||j>=grid1[0].size() ){
+            return true;
+        } 
+        if(grid2[i][j]!=1){
             return true;
         }
+        grid2[i][j]=-1;
+        bool result=grid1[i][j]==1;
+        result=result & substring(grid1,grid2,i+1,j);
+        result=result & substring(grid1,grid2,i,j+1);
+        result=result & substring(grid1,grid2,i-1,j);
+        result=result & substring(grid1,grid2,i,j-1);
         
-        if(grid2[i][j] != 1) { //we only need land
-            return true;
-        }
-
-        grid2[i][j] = -1; //mark visited
-
-        bool result = (grid1[i][j] == 1); //grid1[i][j] must have 1
-
-        result = result & checkSubIsland(grid1, grid2, i+1, j); //Down
-        result = result & checkSubIsland(grid1, grid2, i-1, j); //Up
-        result = result & checkSubIsland(grid1, grid2, i, j+1);  //Right
-        result = result & checkSubIsland(grid1, grid2, i, j-1); //Left
-
         return result;
-
     }
-
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        //DFS
-        int subIslands = 0;
-        int m = grid2.size(); //rows
-        int n = grid2[0].size(); //cols
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid2[i][j] == 1 && checkSubIsland(grid1, grid2, i, j)) { //Found an island
-                    subIslands++;
+        int r=grid2.size();
+        int c=grid2[0].size();
+        int island=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid2[i][j]==1 && substring(grid1,grid2,i,j)){
+                    island++;
                 }
             }
         }
-
-        return subIslands;
+        return island;
     }
 };
-
