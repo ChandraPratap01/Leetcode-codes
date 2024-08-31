f669330cd686c14c15ddb8cb965525df46a1d960
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-09-01 00:07:39 +0530

    Time: 139 ms (25.68%), Space: 73.9 MB (17.64%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    typedef pair<double, int> P;
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        unordered_map<int, vector<pair<int, double>>> adj;
-        vector<double> result(n, 0);
-        
-        for(int i=0;i<edges.size();i++)
-        {
-            int u       = edges[i][0];
-            int v       = edges[i][1];
-            double prob = succProb[i];
-            adj[u].push_back({v, prob});
-            adj[v].push_back({u, prob});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        unordered_map<int,vector<pair<int,double>>>adj;
+        vector<double>result(n,0);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            double prob=succProb[i];
+            adj[u].push_back({v,prob});
+            adj[v].push_back({u,prob});
         }
-
-        priority_queue<P> pq; //max heap because we need maximum probability
-
-		result[start] = 1;
-		pq.push({1.0, start});
-
-		while(!pq.empty()) {
-
-		    int curnode    = pq.top().second;
-            double curProb = pq.top().first;
+        priority_queue<pair<double,int>>pq;
+        result[start_node]=1.0;
+        pq.push({1.0,start_node});
+        while(!pq.empty()){
+            double pqprob=pq.top().first;
+            int pqnode=pq.top().second;
             pq.pop();
-
-            for(auto &child : adj[curnode])
-            {
-                if(result[child.first] < curProb*child.second) //we want maximum this time
-                {
-                    result[child.first] = curProb*child.second;
-                    pq.push({result[child.first], child.first});
+            for(auto &it:adj[pqnode]){
+                int adjnode=it.first;
+                double adjprob=it.second;
+                if(result[adjnode]<pqprob*adjprob){
+                    result[adjnode]=pqprob*adjprob;
+                    pq.push({result[adjnode],adjnode});
                 }
+                
             }
-
-		}
-
-		return result[end];
-
+        }
+         return result[end_node];
     }
 };
