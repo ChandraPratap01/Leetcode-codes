13300182b2bcc83ece59462862d6fa741e79d369
ChandraPratap01 <154074962+ChandraPratap01@users.noreply.github.com>
2024-09-01 21:12:31 +0530

    Time: 94 ms (15.95%), Space: 92.7 MB (25.95%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -5,13 +5,11 @@ public:
         if(original.size()!=n*m){
             return {};
         }
-        int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                result[i][j]=original[count];
-                count++;
-            }
-        }
+       for(int i=0;i<original.size();i++){
+           int row=i/n;
+           int col=i%n;
+           result[row][col]=original[i];
+       }
         return result;
     }
 };
